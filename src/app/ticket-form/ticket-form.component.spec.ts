d5d470010ca5da8df99470c6980c1389cb3c0ebe
David Brady <dgrbrady@gmail.com>
2022-02-09 10:57:00 -0500

    added TicketFormComponent unit tests

--- src/app/ticket-form/ticket-form.component.spec.ts
+++ src/app/ticket-form/ticket-form.component.spec.ts
@@ -1,4 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { TicketFormComponent } from './ticket-form.component';
 
@@ -8,7 +10,8 @@ describe('TicketFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TicketFormComponent ]
+      declarations: [ TicketFormComponent ],
+      imports: [ReactiveFormsModule],
     })
     .compileComponents();
   });
@@ -22,4 +25,38 @@ describe('TicketFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Save Changes Button', () => {
+    it('should ouput createTicket if no Ticket is passed via input', waitForAsync(async () => {
+      const saveButton = fixture.debugElement.query(By.css('button'))
+      const createTicketSpy = spyOn(component.createTicket, 'next');
+      const description = fixture.debugElement.query(By.css('textarea'));
+      description.nativeElement.value = 'asdf';
+      description.nativeElement.dispatchEvent(new Event('input'))
+      fixture.detectChanges();
+      await fixture.whenStable();
+      saveButton.nativeElement.click();
+      fixture.detectChanges();
+      await fixture.whenStable();
+      expect(createTicketSpy).toHaveBeenCalledWith(component.ticketForm.value);
+    }));
+
+    it('should ouput updateTicket if Ticket is passed via input', waitForAsync(async () => {
+      const ticket = {id: 1, description: 'test', assigneeId: 1, completed: true};
+      component.ticket = ticket;
+      fixture.detectChanges();
+      await fixture.whenStable();
+      const saveButton = fixture.debugElement.query(By.css('button'))
+      const updateTicketSpy = spyOn(component.updateTicket, 'next');
+      const description = fixture.debugElement.query(By.css('textarea'));
+      description.nativeElement.value = 'asdf';
+      description.nativeElement.dispatchEvent(new Event('input'))
+      fixture.detectChanges();
+      await fixture.whenStable();
+      saveButton.nativeElement.click();
+      fixture.detectChanges();
+      await fixture.whenStable();
+      expect(updateTicketSpy).toHaveBeenCalledWith(component.ticketForm.value);
+    }));
+  })
 });
