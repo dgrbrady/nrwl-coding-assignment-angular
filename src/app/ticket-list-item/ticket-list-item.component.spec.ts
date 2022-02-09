45797acdc73234e0896b773d9aea72a5bc492ee4
David Brady <dgrbrady@gmail.com>
2022-02-09 10:43:16 -0500

    added TicketListItemComponent unit tests

--- src/app/ticket-list-item/ticket-list-item.component.spec.ts
+++ src/app/ticket-list-item/ticket-list-item.component.spec.ts
@@ -1,14 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 import { TicketListItemComponent } from './ticket-list-item.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-route-test',
+  template: 'app-route-test',
+})
+class RouteTestComponent {}
 
 describe('TicketListItemComponent', () => {
   let component: TicketListItemComponent;
   let fixture: ComponentFixture<TicketListItemComponent>;
+  const ticket = {id: 1, description: 'test', assigneeId: 1, completed: false};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TicketListItemComponent ]
+      declarations: [ TicketListItemComponent, RouteTestComponent,],
+      imports: [RouterTestingModule.withRoutes([
+        {path: 'tickets/:ticketId', component: RouteTestComponent}
+      ])]
     })
     .compileComponents();
   });
@@ -16,10 +30,40 @@ describe('TicketListItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TicketListItemComponent);
     component = fixture.componentInstance;
+    component.ticket = ticket;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the ticket id', () => {
+    const ticketId = fixture.debugElement.query(By.css('.ticket-id'));
+    expect(ticketId.nativeElement.textContent).toContain(component.ticket.id);
+  });
+
+  it('should display the ticket description', () => {
+    const ticketDescription = fixture.debugElement.query(By.css('.ticket-description'));
+    expect(ticketDescription.nativeElement.textContent).toContain(component.ticket.description);
+  });
+
+  it('should display the ticket assigneeId', () => {
+    const ticketAssigneeId = fixture.debugElement.query(By.css('.ticket-assigneeId'));
+    expect(ticketAssigneeId.nativeElement.textContent).toContain(component.ticket.assigneeId);
+  });
+
+  it('should display the ticket completion', () => {
+    const ticketCompleted= fixture.debugElement.query(By.css('.ticket-completed'));
+    expect(ticketCompleted.nativeElement.textContent).toContain(component.ticket.completed);
+  });
+
+  it('should change route when clicking details link', waitForAsync(async () => {
+    const location = TestBed.inject(Location);
+    const detailsLink = fixture.debugElement.query(By.css('.ticket-link'));
+    detailsLink.nativeElement.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(location.path()).toBe(`/tickets/${ticket.id}`);
+  }))
 });
